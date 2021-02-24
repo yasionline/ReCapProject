d1b47f4c40418988d211b34e412fdb0cc18a2a2e
yasin öney <yasin.deniz.06@hotmail.com>
2021-02-24 14:53:17 +0300

    24 Şubat

---
+++ Business/Concrete/RentelManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentelManager : IRentelService
+    {
+        IRentelDal _rentelDal;
+        public RentelManager(IRentelDal rentelDal)
+        {
+            _rentelDal = rentelDal;
+        }
+        public IResult Add(Rentel rentel)
+        {
+            _rentelDal.Add(rentel);
+            return new SuccessResult("Rentel eklendi");
+        }
+
+        public IDataResult<List<Rentel>> GetAll()
+        {
+            return new SuccesDataResult<List<Rentel>>(_rentelDal.GetAll(), "Rentel Listelendi");
+        }
+    }
+}
